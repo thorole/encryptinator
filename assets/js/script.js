dd773bfc12505e3b43654b77fe45b74aee13b49c
Thor-Ole Øby <punkthor@gmail.com>
2020-05-05 18:06:03 +0200

    Made Read more/less buttons change text when clicked.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,29 @@
-$("#intro-button-one").click(function() {
-    $("#intro-more-one").slideToggle(500);
-  });
+$("#intro-button-one").click(function () {
+  if ($(this).text() === "Read more") {
+    $(this).text("Read less");
+  }
+  else {
+    $(this).text("Read more");
+  }
+  $("#intro-more-one").slideToggle(200);
+});
 
-  $("#intro-button-two").click(function() {
-    $("#intro-more-two").slideToggle(500);
-  });
+$("#intro-button-two").click(function () {
+  $("#intro-more-two").slideToggle(200);
+  if ($(this).text() === "Read more") {
+    $(this).text("Read less");
+  }
+  else {
+    $(this).text("Read more");
+  }
+});
 
-  $("#intro-button-three").click(function() {
-    $("#intro-more-three").slideToggle(500);
-  });
+$("#intro-button-three").click(function () {
+  $("#intro-more-three").slideToggle(200);
+  if ($(this).text() === "Read more") {
+    $(this).text("Read less");
+  }
+  else {
+    $(this).text("Read more");
+  }
+});
