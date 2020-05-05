35e830324f76c61cd899d11f332b3e2bada3a012
Thor-Ole Øby <punkthor@gmail.com>
2020-05-05 12:25:26 +0200

    Added 'read more' functionality on intro paragraphs with jquery.

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+$("#intro-button-one").click(function() {
+    $("#intro-more-one").slideToggle(500);
+  });
+
+  $("#intro-button-two").click(function() {
+    $("#intro-more-two").slideToggle(500);
+  });
+
+  $("#intro-button-three").click(function() {
+    $("#intro-more-three").slideToggle(500);
+  });
