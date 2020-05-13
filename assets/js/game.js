febe2993ec3e7406104787e08c1263b69c0e6f45
Thor-Ole Øby <punkthor@gmail.com>
2020-05-13 18:16:39 +0200

    Added cesar function. Removed code to only make it decrypt a word. Added a getWord function for testing.

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,8 +1,41 @@
+
 $(document).ready(function () {
 
-    $("#start-game-btn").click(function() {
-    $("#start-game-btn").addClass("hidden");
-    $(".game-element").removeClass("hidden");
-});
+    let alphabetOriginal = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q",
+        "r", "s", "t", "u", "v", "w", "x", "y", "z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k",
+        "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+    let stageOne = ["car", "cow", "too"];
+    let stageTwo = ["milk", "call", "core"];
+    let stageThree = ["ready", "sauce", "cream"];
+
+    $("#start-game-btn").click(function () {
+        $("#start-game-btn").addClass("hidden");
+        $(".game-element").removeClass("hidden");
+    });
+
+    let gameWord;
+    function getWord(level) {
+        gameWord = level[0];
+    }
+
+    let cesarString = "";
+    let shift = 13;
+    function encryptGameWord(word) {
+        alphabet = alphabetOriginal;
+        cesarString = "";
+        inputText = inputText.toLowerCase();
+        for (let i = 0; i < inputText.length; i++) {
+            for (let j = 0; j < alphabet.length; j++) {
+                if (inputText[i] == alphabet[j]) {
+                    cesarString += alphabet[j + shift];
+                    j = alphabet.length;
+                }
+            }
+        }
+    }
+
+    getWord(stageOne);
+    console.log("Gameword: " + gameWord);
 
 });
