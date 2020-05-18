fa648f7530e9e8be68be7ceb32496de5ce6fff86
Thor-Ole Øby <punkthor@gmail.com>
2020-05-18 21:48:44 +0200

    Set startTimer to 60 sec.

--- assets/js/game.js
+++ assets/js/game.js
@@ -40,7 +40,7 @@ $(document).ready(function () {
         encryptGameWord(gameWord);
         writeEncryptedWord();
         toggleGameElements();
-        startTimer(5);
+        startTimer(60);
     }
 
     function initiateGame() {
