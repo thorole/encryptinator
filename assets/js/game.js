44238bd635b0371babf3c56172206f2076883a5a
Thor-Ole Øby <punkthor@gmail.com>
2020-05-21 20:44:30 +0200

    Removed commented out duplicate of alphabetOriginal variable.

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,10 +1,6 @@
 
 $(document).ready(function () {
 
-    /*let alphabetOriginal = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q",
-        "r", "s", "t", "u", "v", "w", "x", "y", "z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k",
-        "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];*/
-
     let stages = [
         ["car", "cow", "too", "far", "see", "sea", "all", "say"],
         ["milk", "call", "core", "fish", "must", "mill", "mark", "quit"],
