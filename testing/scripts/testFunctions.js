94b13dc847245310d50dd3439f002246e6a2e277
Thor-Ole Øby <punkthor@gmail.com>
2020-06-04 18:09:58 +0200

    Added testing of vigenere encryption function.

--- testing/scripts/testFunctions.js
+++ testing/scripts/testFunctions.js
@@ -28,3 +28,34 @@ function iterateString(text, shiftNumber, reversed) {
     }
     return cesarString;
 }
+
+function iterateVigenereString(text, numberKey, reverse) {
+    let vigenereText;
+    let key = numberKey;
+    console.log("text is: " + text);
+    let alphabet = getAlphabet();
+    vigenereText = "";
+    if (reverse === true) {
+        alphabet = alphabet.reverse();
+    }
+    let inputText = text;
+    console.log("userstring is: " + inputText);
+    for (let i = 0, g = 0; i < inputText.length; i++) {
+        if (g === key.length) {
+            g = 0;
+        }
+        for (let j = 0; j < alphabet.length; j++) {
+            if (inputText[i] === alphabet[j]) {
+                vigenereText += alphabet[j + key[g]];
+                j = alphabet.length;
+                g++;
+                console.log("encryptedmsginloopis: " + vigenereText);
+            }
+            if (!(/^[a-z]/).test(inputText[i])) {    //Makes sure characters not in the range of a-z is printed
+                vigenereText += inputText[i];
+                break;
+            }
+        }
+    }
+    return vigenereText;
+}
--- testing/spec/functionSpecs.js
+++ testing/spec/functionSpecs.js
@@ -1,28 +1,37 @@
+//Test cesar cipher function
+
 describe("Encrypts or decrypts text", function() {
     let shift = 3;
     let text = "correct";
     let decryptedText = "zloobzq";
-    let decrypt = true;
-    let encrypt = false;
+    let decrypt = false;
+    let encrypt = true;
 
     it("should return zloobzq", function () {
-        expect(iterateString(text, shift, decrypt)).toBe("zloobzq");
+        expect(iterateString(text, shift, encrypt)).toBe("zloobzq");
     });
     it("should return correct", function () {
-        expect(iterateString(decryptedText, shift, encrypt)).toBe("correct");
+        expect(iterateString(decryptedText, shift, decrypt)).toBe("correct");
     });
 
 });
 
-/*describe("The Drink Game", function () {
-    it("should return error!", function () {
-        expect(whatCanIDrink(0)).toBe("Sorry. I can't tell what drink because that age is incorrect");
-      
+//Test Vigenere cipher encryption/decryption function
+
+describe("Encrypts or decrypts text", function() {
+    let key = [1,2,3];
+    let text = "correct";
+    let decryptedText = "dqusgfu";
+    let decrypt = true;
+    let encrypt = false;
+
+    it("should return dqusgfu", function () {
+        expect(iterateVigenereString(text, key, encrypt)).toBe("dqusgfu");
     });
-    it("should return error!", function() {
-        expect(whatCanIDrink("hei")).toBe("Sorry. I can't tell what drink because that age is incorrect");
+    it("should return correct", function () {
+        expect(iterateVigenereString(decryptedText, key, decrypt)).toBe("correct");
     });
-    it("should return drink beer", function() {
-        expect(whatCanIDrink(19)).toBe("Drink Beer");
-    });
-});*/
+
+});
+
+
