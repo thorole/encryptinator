6f20fc4d449b66d3d193727220a33a303e4e2845
Thor-Ole Øby <punkthor@gmail.com>
2020-06-13 20:12:01 +0200

    Generalized read more/less button handlers by using data attributes.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,11 @@
 "use strict";
 
 /*
-Causes read more button to show read less when clicked, and vica versa. 
+Causes read more button to show read less/read more and display/hide hidden section when clicked. 
 */
-$("#intro-button-one").click(function () { 
-  $("#intro-more-one").slideToggle(200);
-  if ($(this).text() === "Read more") {
-    $(this).text("Read less");
-  }
-  else {
-    $(this).text("Read more");
-  }
-});
-
-/*
-Causes read more button to show read less when clicked, and vica versa. 
-*/
-$("#intro-button-two").click(function () { 
-  $("#intro-more-two").slideToggle(200);
+$(".read-more").click(function () { 
+  let hiddenTextId = this.getAttribute("data-display");
+  $(hiddenTextId).slideToggle(200);
   if ($(this).text() === "Read more") {
     $(this).text("Read less");
   }
@@ -38,7 +26,6 @@ $(".close").click(function () {
   let modalId = this.getAttribute("data-open");
   $(modalId).css("display", "none");
 });
-
 
 $(".modal").click(function () {
   $(this).css("display", "none");
